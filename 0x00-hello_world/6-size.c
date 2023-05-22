abc392100cc65d1efbbf167cf9812d35341450cb
hydaline-code <djougancharlen@gmail.com>
2023-05-22 20:27:15 +0100

    Print size of types check

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
-#include<stdio>
+#include<stdio.h>
 /**
  * main - Entry point
  *
  * Return: always 0
  */
-int main(0)
+int main(void)
 {
 	printf("Size of char is: %ld", sizeof(char)"bytes");
 	printf("Size of int is: %ld", sizeof(int)"bytes");
