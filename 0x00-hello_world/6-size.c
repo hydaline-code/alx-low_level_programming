5e2ef57029c2abe6fde2d93929dddce2165d486d
hydaline-code <djougancharlen@gmail.com>
2023-05-22 20:21:24 +0100

    Print size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(0)
+{
+	printf("Size of char is: %ld", sizeof(char)"bytes");
+	printf("Size of int is: %ld", sizeof(int)"bytes");
+	printf("Size of short is: %ld", sizeof(short)"bytes");
+	printf("Size of long is: %ld", sizeof(long)"bytes");
+	return (0);
+}
