bfc54a9d53de06848c52b42b3516bb5278e0c75d
hydaline-code <djougancharlen@gmail.com>
2023-05-22 17:56:08 +0100

    First program check4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	char str[51];
 
-	strcpy(str, "Programming is like building a multilingual puzzle");
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
 	puts(str);
 	return (0);
 }
