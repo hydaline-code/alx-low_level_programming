26686b038fa0c6683e337bd68840bb85b8588314
hydaline-code <djougancharlen@gmail.com>
2023-05-23 13:59:13 +0100

    Comparing numbers bettychck4

--- 0x01-variable_if_else_while/0-positive_or_negative.c
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -12,15 +13,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{	printf("%d is positive\n", n);
+	if (n < 0)
+	{	printf("%d is negative\n", n);
 	}
 	else if (n == 0)
 	{	printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
