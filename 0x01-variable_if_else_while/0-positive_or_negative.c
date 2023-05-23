e673e9972638b29afa0c1631141e523df1899f2d
hydaline-code <djougancharlen@gmail.com>
2023-05-23 13:45:15 +0100

    Comparing numbers bettychck1

--- 0x01-variable_if_else_while/0-positive_or_negative.c
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -12,14 +12,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{	printf("%d : is positive");
+	{	printf("%d is positive");
 	}
 	else if (n == 0)
-	{	printf("%d : is zero");
+	{	printf("%d is zero");
 	}
 	else
 	{
-		printf("%d : is negative");
+		printf("%d is negative");
 	}
 	return (0);
 }
