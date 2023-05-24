43e625506f0a14a4156f9215be484b44504015af
hydaline-code <djougancharlen@gmail.com>
2023-05-24 09:13:53 +0100

    Print both lower and uppercase letters check2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,14 @@
 int main(void)
 {
 	char letter;
-	char letter_U;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
-	for (letter_U = 'A'; letter_U <= 'z'; letter_U++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
-		putchar(letter_U);
+		putchar(letter);
 	}
 	putchar('\n');
 	return (0);
