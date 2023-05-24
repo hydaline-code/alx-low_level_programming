7e99a7335a17457e8aadce05bc1a93c7aaae8cf3
hydaline-code <djougancharlen@gmail.com>
2023-05-24 13:56:27 +0100

    Printing in base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,13 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 16; n++)
+	for (n = 0; n < 10; n++)
 	{
 		putchar(48 + n);
+	}
+	for (n = 10; n < 16; n++)
+	{
+		putchar(87 + n);
 	}
 	putchar('\n');
 	return (0);
