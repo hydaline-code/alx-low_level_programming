9a2947cc7021f3b635e3a55f63a2e43cf5bf5c0f
hydaline-code <djougancharlen@gmail.com>
2023-05-24 08:50:17 +0100

    Lowercase alphabet check

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - entry point
+ *
+ *
+ * Return: always return 0
+ *
+ */
 int main(void)
 {
 	char letter;
-	
-	for (letter='a'; letter<='z'; letter++)
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
