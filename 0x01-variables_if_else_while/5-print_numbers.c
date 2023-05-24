a80380071204c67ff17078c8a0ceb557908782db
hydaline-code <djougancharlen@gmail.com>
2023-05-24 10:41:52 +0100

    Digits of base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return:always return 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	   printf("%d\n", n);
+	}
+	return (0);
+}
