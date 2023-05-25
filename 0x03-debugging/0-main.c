23640d3a123e2ccbd51a0a36b3f1124b107f927b
hydaline-code <djougancharlen@gmail.com>
2023-05-25 19:23:42 +0100

    Debugging test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,7 +9,17 @@ int main(void)
 	 int i;
 
 	 i = 98;
-	positive_or_negative(i);
-
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
 	return (0);
 }
