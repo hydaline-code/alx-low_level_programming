1fedd9168f043bf101f845b7fe5e932299e4001a
hydaline-code <djougancharlen@gmail.com>
2023-05-25 10:05:38 +0100

    Modify the header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef main.h
-#define main.h
+#ifndef  _MAIN_H_
+#define  _MAIN_H_
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +13,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
 #endif 
