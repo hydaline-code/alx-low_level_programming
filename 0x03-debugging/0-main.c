9dae442641566a0d1bd2fcc05d7955df52e90165
hydaline-code <djougancharlen@gmail.com>
2023-05-25 18:51:25 +0100

    Betty style check

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /**
 * main - tests function that prints if integer is positive or negative
@@ -7,24 +7,23 @@
 */
 
 int main(void)
-  {
-          int n;
-  
-         srand(time(0));
-         n = rand() - RAND_MAX / 2;
- 
-         if (n > 0)
-         {
-                 printf("%d is positive\n", n);
-         }
-         else if (n == 0)
-         {
-                printf("%d is zero\n", n);
-         }
-         else
-         {
-                 printf("%d is negative\n", n);
-         }
- 
-         return (0);
- }
+{
+	int n;
+
+	 srand(time(0));
+	 n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
