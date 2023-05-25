6a7a3037418a0599276c8658bee237e93671cbf3
hydaline-code <djougancharlen@gmail.com>
2023-05-25 03:46:15 +0100

    New fil creation for task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stlib.h>
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+int main(0)
+{
+	printf("_putchar\n");
+	return (0);
+}
