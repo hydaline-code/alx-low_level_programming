bcbcbd5bcd72ac115d300e10a8c56208a2392206
hydaline-code <djougancharlen@gmail.com>
2023-05-25 03:54:37 +0100

    New fil creation for task0 check

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,14 @@
  */
 int main(0)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
