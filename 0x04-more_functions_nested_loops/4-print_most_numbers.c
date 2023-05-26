0a897f034afa7ba07f010b59b6dc3ccf569f76ea
hydaline-code <djougancharlen@gmail.com>
2023-05-26 23:16:20 +0100

    Task4 functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
+/**
+ * print_most_numbers - prints the numbers
+ *
+ */
 void print_most_numbers(void)
 {
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
-	{	if (i != '2' && i != '4')
+	{
+		if (i != '2' && i != '4')
 		{
 			_putchar(i);
 		}
