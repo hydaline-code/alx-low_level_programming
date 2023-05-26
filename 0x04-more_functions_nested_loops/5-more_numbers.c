59641acd67a1c2344a6200b5739e2e63827a82de
hydaline-code <djougancharlen@gmail.com>
2023-05-27 00:17:27 +0100

    Task5 check

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,14 @@ void more_numbers(void)
 
 	for (cnt = 0; cnt < 10; cnt++)
 	{
-		for (i = '0'; i <= 14; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i);
+			if (i > 9)
+			{
+				_putchar ((i / 10) + '0');
+			}
+				_putchar ((i % 10) + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
