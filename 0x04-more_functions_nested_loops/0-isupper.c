dbf0f2a939c915802f11905b402cd766f0fba153
hydaline-code <djougancharlen@gmail.com>
2023-05-26 12:27:34 +0100

    Updated the function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype>
+/**
+ * _isupper - determine if uppercase
+ *
+ * @c: character to determine
+ *
+ * Return: gives boolean
+ */
+
+int _isupper(int c)
+{
+	for (c >= 'A' && c <= 'Z')
+	{
+		if (_isupper(c))
+		{
+			_putchar(49);
+		}
+		else
+		{
+			_putchar(48);
+		}
+	}
+}
