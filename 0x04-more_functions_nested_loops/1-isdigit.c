e1ae86fa33427754ebedee1d3ea2067cf4ccf787
hydaline-code <djougancharlen@gmail.com>
2023-05-26 13:06:36 +0100

    Check isdigit function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - prints digit
+ * @c: character to check
+ *
+ * Return: gives 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c = '0' && c = '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
