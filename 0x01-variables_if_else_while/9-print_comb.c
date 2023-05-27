f815b3e841b64ddea37e2354e87e3f55afeaa74b
hydaline-code <djougancharlen@gmail.com>
2023-05-27 21:58:04 +0100

    Task9check

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,21 +5,20 @@
  * Return: alway returns 0
  *
  */
+
 int main(void)
 {
-	int i=0;
+	int i;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(48 + i);
-
-			if (i !=  9)
+			if (i != 9)
 			{
 				putchar(',');
 				putchar(32);
 			}
-			i++;
 	}
+	putchar('$');
 	return (0);
 }
-
