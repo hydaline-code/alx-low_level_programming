39858634ba149277053dc035faff3fb815f06643
hydaline-code <djougancharlen@gmail.com>
2023-05-28 12:13:02 +0100

    Task4 bettycheck

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
  * Return: 1 if true, 0 if false
  *
  */
- int _isalpha(int c)
- {
- 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
