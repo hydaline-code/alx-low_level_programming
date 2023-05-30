1d69ad449697b9202cd0da80d9fa0e3013a521a6
hydaline-code <djougancharlen@gmail.com>
2023-05-30 13:32:33 +0100

    Updated main.h file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef  _MAIN_H_
 #define  _MAIN_H_
+#include <unistd.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -11,4 +12,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
 #endif
