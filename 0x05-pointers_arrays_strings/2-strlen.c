e925ccd17ab611299c28473ea7772a921384b90b
hydaline-code <djougancharlen@gmail.com>
2023-05-31 01:22:51 +0100

    Task2 check

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+
+int _strlen(char *s)
+{
+
+	int i;
+	int j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j++;
+	}
+	return (j);
+}
