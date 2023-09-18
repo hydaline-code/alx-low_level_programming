344ab6a46dbd7115f2f4fce16d8e3c0ec3d96ab7
hydaline-code <djougancharlen@gmail.com>
2023-09-18 15:42:04 +0100

    Add 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,12 @@
  * @str: string to print
  */
 
-void _puts(char* str) {
+void print_rev(char* str) {
     int i;
 
-    for (i = _strlen(str) - 1; i >= 0; i--) {
+    for (i = strlen(str) - 1; i >= 0; i--) {
         putchar(str[i]);
     }
     putchar('\n');
 }
+
